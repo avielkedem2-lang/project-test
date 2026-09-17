435a9a3583f917d9eeb97edee66ef9cfc15e3de7
אביאל קדם <avielkedem2@gmail.com>
2026-09-17 12:44:47 +0300

    add function sendRequestGet

--- front/my-app/src/fetch.ts
+++ front/my-app/src/fetch.ts
@@ -1,20 +1,24 @@
 import axios from "axios";
 
 
+type User = {
+    name: string,
+    email: string,
+    _id: string
+}
 
 
 
-
-async function sendRequestPost(url:string, body: object) {
+async function sendRequestPost(url: string, body: object) {
     try {
-        const {data, status} = await axios.post(url, body);
-        return  {data, status}
+        const { data, status } = await axios.post(url, body);
+        return { data, status }
     } catch (err) {
-        if (axios.isAxiosError(err)){
+        if (axios.isAxiosError(err)) {
             const message = err.response?.data
             console.log(message);
             const status = err.status
-            return {message, status}
+            return { message, status }
         }
     }
 }
@@ -30,8 +34,36 @@ export async function register(body: object) {
 
 
 
-export async function login(body:object) {
+export async function login(body: object) {
     const url = "http://localhost:3000/login";
     const res = await sendRequestPost(url, body)
+    return res
+}
+
+
+
+
+
+async function sendRequestGet(url: string, token: string) {
+    try {
+        const { data } = await axios.get<User>(url, { headers: { token } })
+        return data
+    } catch (err) {
+        if (axios.isAxiosError(err)) {
+            const message = err.response?.data
+            console.log(message);
+            const status = err.status
+            return { message, status }
+        }
+    }
+}
+
+
+
+
+async function getUser(token: string) {
+    // const token = localStorage.getItem("token")
+    const url = "http://localhost:3000/user";
+    const res = await sendRequestGet(url, token)
     return res
 }
