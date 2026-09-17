7f67052c0ea7600bd8d1e95c5db7175d3df20e31
אביאל קדם <avielkedem2@gmail.com>
2026-09-17 11:54:07 +0300

    create basic fatch(Axios)

---
+++ front/my-app/src/fetch.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+
+
+
+
+
+async function sendRequestPost(url:string, body: object) {
+    try {
+        const {data, status} = await axios.post(url, body);
+        return  {data, status}
+    } catch (err) {
+        if (axios.isAxiosError(err)){
+            const message = err.message
+            console.log(message);
+            const status = err.status
+            return {message, status}
+        }
+    }
+}
+
+
+
+
+export async function register(body: object) {
+    const url = "http://localhost:3000/register";
+    const res = await sendRequestPost(url, body);
+    return res
+}
+
+
+
+export async function login(body:object) {
+    const url = "http://localhost:3000/login";
+    const res = await sendRequestPost(url, body)
+    return res
+}
