7a6c659949a2921d87c34eeea9d092218ecbb241
אביאל קדם <avielkedem2@gmail.com>
2026-09-17 12:35:14 +0300

    finsh page Register

--- front/my-app/src/components/pages/Register.tsx
+++ front/my-app/src/components/pages/Register.tsx
@@ -1,6 +1,40 @@
+import { useRef, useState } from "react"
+import { register } from "../../fetch"
+import { useNavigate } from "react-router"
+
+type UserRegister = {
+  name: string
+  email: string,
+  password: string
+}
+
 
 export default function Register() {
+  const user = useRef<UserRegister>({ name: "", email: "", password: "" })
+  const navigate = useNavigate()
+  const isError = useRef<boolean>(false)
+  const [error, setError] = useState<string>()
   return (
-    <div>Register</div>
+    <div>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        register(user.current).then((data)=> {
+          if (data?.status === 201){
+            navigate("/login")
+          }
+          isError.current = true
+          setError(data?.message.message)
+        })
+      }}>
+        <input type="text" placeholder="Enter name" onChange={(e) => user.current = { ...user.current, name: e.target.value }} />
+        <input type="email" placeholder="Enter email" onChange={(e) => user.current = { ...user.current, email: e.target.value }} />
+        <input type="password" placeholder="*******" onChange={(e) => user.current = { ...user.current, password: e.target.value }} />
+        <button type="submit">submit</button>
+        <br />
+        {isError && (
+          <p>{error}</p>
+        )}
+      </form>
+    </div>
   )
 }
